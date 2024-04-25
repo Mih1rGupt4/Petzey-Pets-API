d48142b3d7f72656877645722a54a5d93c416b59
Mih1rGupt4 <mihirgupta422@gmail.com>
2024-04-25 17:30:27 +0530

    add api methods

--- Petzey/Petzey.WebAPI/Controllers/PetsController.cs
+++ Petzey/Petzey.WebAPI/Controllers/PetsController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Petzey.Data.Repository;
+using Petzey.Domain.Entities;
+using Petzey.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +12,29 @@ namespace Petzey.WebAPI.Controllers
 {
     public class PetsController : ApiController
     {
+        IPetsRepository repo = new PetsRepository();
+
+        [HttpGet]
+        public IHttpActionResult GetPetDetailsByPetID(int id)
+        {
+            Pet pet = repo.GetPetDetailsByPetID(id);
+            return OkOrNotFound(pet);
+        }
+
+        [HttpPost]
+        public IHttpActionResult GetMorePets(int pageNumber, int pageSize = 10) // Default pageSize is 10
+        {
+            List<Pet> pets = repo.GetMorePets(pageNumber, pageSize);
+            return Ok(pets);
+        }
+
+
+        private IHttpActionResult OkOrNotFound(object pet)
+        {
+            if(pet == null)
+                return NotFound();
+            else
+                return Ok(pet);
+        }
     }
 }
