58b0ca9d30362d7c8a6d551ea2c6194f7b0d5d8d
Mih1rGupt4 <mihirgupta422@gmail.com>
2024-04-25 17:29:35 +0530

    add repo methods

--- Petzey/Petzey.Data/Repository/PetsRepository.cs
+++ Petzey/Petzey.Data/Repository/PetsRepository.cs
@@ -1,6 +1,8 @@
-﻿using Petzey.Domain.Interfaces;
+﻿using Petzey.Domain.Entities;
+using Petzey.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,22 @@ namespace Petzey.Data.Repository
 {
     public class PetsRepository : IPetsRepository
     {
+        PetzeyPetsDbContext db;
+        public PetsRepository()
+        {
+            db = new PetzeyPetsDbContext();
+        }
+
+        public Pet GetPetDetailsByPetID(int id)
+        {
+            return db.Pets.FirstOrDefault(p => p.PetID == id);
+        }
+
+        public List<Pet> GetMorePets(int pageNumber, int pageSize)
+        {
+            // Add OrderBy before Skip
+            return db.Pets.OrderBy(p => p.PetID).Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            //return db.Pets.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+        }
     }
 }
