1f9c1fe07dfcba65bddeeded36f255a07fb25cde
Shashank Joshi <sj.shashankjoshi.sj@gmail.com>
2024-04-25 18:35:59 +0530

    API edit and add

--- Petzey/Petzey.WebAPI/Controllers/PetsController.cs
+++ Petzey/Petzey.WebAPI/Controllers/PetsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Petzey.Data.Repository;
+using Petzey.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,27 @@ namespace Petzey.WebAPI.Controllers
 {
     public class PetsController : ApiController
     {
+        public PetsRepository repo = new PetsRepository();
+        [HttpPost]
+        public IHttpActionResult AddPet([FromBody] Pet pet)
+        {
+            var newPet = repo.AddPet(pet);
+            if(newPet != null)
+            {
+                return Ok("New Pet added");
+            }
+            return BadRequest("Pet not added!");
+        }
+
+        [HttpPut]
+        public IHttpActionResult EditPet([FromBody] Pet pet)
+        {
+            var editPet = repo.EditPet(pet);
+            if(editPet != null)
+            {
+                return Ok("edit successfull");
+            }
+            return BadRequest("edit unsuccessfull");
+        }
     }
 }
