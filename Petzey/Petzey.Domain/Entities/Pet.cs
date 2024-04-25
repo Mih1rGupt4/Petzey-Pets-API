2de6807edfe26fbd1d616b98e2a320b1e70e0744
Shashank Joshi <sj.shashankjoshi.sj@gmail.com>
2024-04-25 11:43:59 +0530

    Pet entity class

---
+++ Petzey/Petzey.Domain/Entities/Pet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petzey.Domain.Entities
+{
+    internal class Pet
+    {
+        public int PetID { get; set; }
+        public int PetParentID { get; set; }
+        public string PetName { get; set; }
+        public string Species { get; set; }
+        public string Breed { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Age { get; set; }
+        public string BloodGroup { get; set; }
+        public string Allergies { get; set; }
+    }
+}
