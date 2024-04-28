5c48c73a9c1f4b527fa071dc53eeea9557f4356e
Shashank Joshi <sj.shashankjoshi.sj@gmail.com>
2024-04-28 23:13:45 +0530

    comment commit

--- Petzey/Petzey.WebAPI.UnitTest/PetzeyWebAPIUnitTests.cs
+++ Petzey/Petzey.WebAPI.UnitTest/PetzeyWebAPIUnitTests.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Petzey.Data.Repository;
+using Petzey.Domain.Entities;
 using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Petzey.WebAPI.Controllers;
 
 namespace Petzey.WebAPI.UnitTest
 {
     [TestClass]
     public class PetzeyWebAPIUnitTests
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
+        //[TestMethod]
+        //public async Task GetAllPets_WithValidInputs_ReturnsOk()
+        //{
+        //    var mockRepo = new Mock<PetsRepository>();
+        //    mockRepo.Setup(repo => repo.GetAllPetsAsync()).ReturnsAsync(new List<Pet> { new Pet { PetID = 1, PetParentID = 2,PetName="test_name", Age = "2" } });
+        //    var controller = new PetsController();
+        //    var result = await controller.GetAllPets(
 
-        }
+        //}
     }
 }
