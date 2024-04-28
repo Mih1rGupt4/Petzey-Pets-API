33361754743445fbadb4b4f84d2654bb335ecd6e
Shashank Joshi <sj.shashankjoshi.sj@gmail.com>
2024-04-29 00:35:07 +0530

    test-methods -added for get all pets

--- Petzey/Petzey.WebAPI.UnitTest/PetzeyWebAPIUnitTests.cs
+++ Petzey/Petzey.WebAPI.UnitTest/PetzeyWebAPIUnitTests.cs
@@ -109,5 +109,45 @@ namespace Petzey.WebAPI.UnitTest
         }
 
 
+        [TestMethod]
+        public async Task GetAllPets_WithPets_ReturnOk()
+        {
+            var mockRepo = new Mock<PetsRepository>();
+            mockRepo.Setup(repo => repo.GetAllPetsAsync()).ReturnsAsync(new List<Pet> { new Pet { PetID = 1, PetParentID = 2, Species = "Dog", Breed = "Pug", PetName = "name_test", Age = "2" } });
+
+            var controller = new PetsController(mockRepo.Object);
+            var result = await controller.GetAllPets();
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkNegotiatedContentResult<List<Pet>>));
+
+            var contentResult = result as OkNegotiatedContentResult<List<Pet>>;
+            Assert.IsNotNull(contentResult.Content);
+            Assert.AreEqual(1, contentResult.Content.Count);
+            Assert.AreEqual("name_test", contentResult.Content.First().PetName);
+            Assert.AreEqual(1, contentResult.Content.First().PetID);
+            Assert.AreEqual(2, contentResult.Content.First().PetParentID);
+            Assert.AreEqual("Dog", contentResult.Content.First().Species);
+            Assert.AreEqual("Pug", contentResult.Content.First().Breed);
+            Assert.AreEqual("2", contentResult.Content.First().Age);
+
+        }
+
+        [TestMethod]
+        public async Task GetAllPets_WithoutPets_ReturnsBadRequest()
+        {
+            //Arrange
+            var mockRepo = new Mock<PetsRepository>();
+            mockRepo.Setup(repo => repo.GetAllPetsAsync()).ReturnsAsync(new List<Pet>());
+            var controller = new PetsController(mockRepo.Object);
+
+            // Act
+            var result = await controller.GetAllPets();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+
+
     }
 }
