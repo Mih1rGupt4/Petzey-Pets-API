0670ba5e8ac3ca9e960e135272880a577e8027a6
Mih1rGupt4 <mihirgupta422@gmail.com>
2024-04-28 20:34:08 +0530

    Add unit test project

---
+++ Petzey/Petzey.WebAPI.UnitTest/PetzeyWebAPIUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Petzey.WebAPI.UnitTest
+{
+    [TestClass]
+    public class PetzeyWebAPIUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Petzey/Petzey.WebAPI.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Petzey.WebAPI.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Petzey.WebAPI.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("daafd86d-9dfd-47bf-bf5b-f515e7e7dcd2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
